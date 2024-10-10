793ee00ad43896c47e2b27a11121199f45e33e6c
yusef carr <ycarr99@gmail.com>
2024-10-10 15:54:38 -0400

    pushing openai

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config(); 
+
 const cors = require('cors');
+
+// Api Key
+const API_KEY = process.env.API_KEY
+
+
 
 // Initialize Express
 const app = express();
@@ -84,6 +90,28 @@ app.use('/rewards', rewardsRoutes);
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.post('/completions', async (req, res) => {
+  const options = {
+      method: "POST",
+      headers: {
+          "Authorization": `Bearer ${API_KEY}`,
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+          model : "gpt-4o-mini",
+          messages: [{ role: "user", content: req.body.message}],
+          max_tokens: 100,
+      })
+  }
+  try {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+      const data = await response.json()
+      res.send(data)
+  } catch (error) {
+      console.error(error)
+  }
+})
 
 // Set up the server
 const PORT = process.env.PORT || 5000;
